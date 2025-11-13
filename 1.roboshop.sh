2d9a7b4d6098ee0ec52a2e90ceffbdbaf57e9b17
harika dharavath <harika@joindevops.com>
2025-11-13 21:34:34 +0530

    message

--- 1.roboshop.sh
+++ 1.roboshop.sh
@@ -11,8 +11,10 @@ do
 #get private ip address of the instance
     if [ $instance != "frontend"] then
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+     
     else  
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)   
+    
     fi
 
     echo "Instance $instance launched with IP address: $IP"
